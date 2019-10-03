6cc504ecf882e041448476e4e110bb09a949feb9
Nathan Keyes <n.keyes1994@gmail.com>
2019-10-03 15:39:03 -0400

    Working game

--- react-game/src/App.js
+++ react-game/src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import PictureCard from "./components/PictureCard";
+import Wrapper from "./components/Wrapper";
+import cards from "./cards.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // Setting this.state.cards to the cards json array
+  state = {
+    cards,
+    clickedArray: [],
+    topScore: 0,
+    score: 0,
+    message: "",
+    shakeit: "false"
+  };
+  clickPicture = id => {
+    // Arrange the pictures in a random manner
+    const shuffledArray = this.shuffleArray(cards);
+    this.setState({cards: shuffledArray});
+    // if clicked an image already clicked set this.state.score = 0; empty clickeadArray, end of if block
+    if (this.state.clickedArray.includes(id)) {
+      this.setState({ score: 0, clickedArray: [], message: "Nope. Click to restart", shakeit: "true"});
+    }
+    else {
+      this.setState({
+        clickedArray: this.state.clickedArray.concat([id]),
+        score: this.state.score + 1,
+        message: "Good -- Keep going",
+        shakeit: "false"
+      });
+    }
+    // set topscore = score if score>topscore.
+    if (this.state.score > this.state.topScore) {
+      this.setState({ topScore: this.state.score });
+    }
+    // shake the wrapper if shakeit is set to true
+  }
+  shuffleArray = (picturesArray) => {
+      for (let i = picturesArray.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [picturesArray[i], picturesArray[j]] = [picturesArray[j], picturesArray[i]];
+      }
+      return picturesArray;
+  }
+  render() {
+    return (
+      <div className="App">
+        <h3 className="App-intro">
+          <strong>Click cards to earn points. If you click the same card more than once, you lose</strong> 
+          <p className = "score"><strong>Score: {this.state.score} | TopScore: {this.state.topScore}</strong></p>
+          <p className="message"><strong>{this.state.message}</strong></p>
+        </h3>
+        <Wrapper
+        shakeWrapper = {this.state.shakeit}
+        pictures=
+          {this.state.cards.map(picture => (
+            <PictureCard
+              clickPicture={this.clickPicture}
+              id={picture.id}
+              key={picture.id} // to get rid of unique key prop warning
+              name={picture.name}
+              image={picture.image}
+            />
+          ))}
+        />
+        <footer className="footer">
+      <div className="container">
+        <span className="text-muted">&Clicky Food</span>
+      </div>
+    </footer> 
+      </div>
+    );
+  }
 }
 
 export default App;
