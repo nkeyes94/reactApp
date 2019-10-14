fb4b41c82914fb9c9ec70939567c99e03ec249f6
Nathan Keyes <n.keyes1994@gmail.com>
2019-10-14 12:47:23 -0400

    Updates

---
+++ precache-manifest.19992d8ac1e726b6db2eab35f2a43277.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02c25e172d3c36062693630c270e5e4e",
+    "url": "/reactApp/index.html"
+  },
+  {
+    "revision": "ed2d5af14af761beb658",
+    "url": "/reactApp/static/css/main.b7f1e15f.chunk.css"
+  },
+  {
+    "revision": "6de675b1a1093657a39e",
+    "url": "/reactApp/static/js/2.623d7711.chunk.js"
+  },
+  {
+    "revision": "ed2d5af14af761beb658",
+    "url": "/reactApp/static/js/main.088344f4.chunk.js"
+  },
+  {
+    "revision": "ec48383320ac0a77b028",
+    "url": "/reactApp/static/js/runtime-main.71ea39a5.js"
+  }
+]);
